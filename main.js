3f61cb943e662abb3cd3d419cde52101e3144c70
Sakis T. <106918030+AthTop@users.noreply.github.com>
2024-02-05 21:15:14 +0200

    Fix bugs, console version done

--- main.js
+++ main.js
@@ -4,15 +4,29 @@
 // has a function to place markers on the board
 
 const gameBoard = (function(){
-    let boardArray = [['', '', '',], [], []];
+    const boardArray = [[], [], []];
     const placeMarker = function(marker, position) {
         let [x, y] = [position[0] -1, position[1] - 1];
         boardArray[x][y] = marker;
     };
-    const board = function() {
+    const getBoard = function() {
         return boardArray.map((v) => (v));
     };
-    return { placeMarker, board };
+    const boardCells = function() {
+        let cells = 0;
+        boardArray.forEach((row) => {
+            row.forEach((item) => {
+                if(item === 'X' || item === 'O') {
+                    cells++;
+                };
+            });
+        });
+        return cells;
+    };
+    const availableCells = function() {
+        return (9 - boardCells());
+    };
+    return { placeMarker, getBoard, availableCells };
 })();
 
 // player object
@@ -27,50 +41,60 @@ function player(name, marker) {
 const game = (function(){
     //Ask player for their name and marker of choice
     const createPlayer = function() {
-        let playerName = prompt("What's your name?");
-        let playerMarker = prompt("What's your marker? O or X");
+        const playerName = prompt("What's your name?");
+        const playerMarker = prompt("What's your marker? O or X");
         return player(playerName, playerMarker.toUpperCase());
     };
     // Ask player where to place the marker
     const playerChoice = function() {
-        let playerChoiceX = prompt("Where to place the marker? Row 1 to 3");
-        let playerChoiceY = prompt("Which column? 1-3");
+        const playerChoiceX = prompt("Where to place the marker? Row 1 to 3");
+        const playerChoiceY = prompt("Which column? 1-3");
         return [playerChoiceX, playerChoiceY];
     };
     // Take a turn, asking the player where to place marker and place it in the gameboard
     const turn = function(player) {
         gameBoard.placeMarker(player.marker, playerChoice());
     };
-    // Game loop
-    const gameLoop = function() {
-        let player1 = createPlayer();
-        do {
-            turn(player1);
-        }
-        while (!winCheck(player1.marker));
-        console.log(`${player1.name} wins.`);
+    // Play a round
+    const playRound = function() {
+        const player1 = createPlayer();
+        const player2 = createPlayer();
+        let activePlayer = player1;
+        const switchPlayer = () => {
+            activePlayer = activePlayer === player1 ? player2 : player1;
+        };
+        while(true) {
+            console.table(gameBoard.getBoard());
+            turn(activePlayer);
+            if(isWin(activePlayer)) {
+                console.log(`${activePlayer.name} wins!`);
+                break;
+            };
+            // Check if all available cells are occupied for a tie
+            if (isTie()) break;
+            switchPlayer();
+        };
     };
     // Winning condition
-    const winCheck = function(marker) {
-        // Helper functions
+    const isWin = function(player) {
         // Helper function to compare elements in array to marker
         const compareMarker = function(array) {
             let count = 0;
             for (let i = 0; i < array.length; i++) {
-                if (array[i] === marker) count++;
+                if (array[i] === player.marker) count++;
                 if (count === 3) return true;
             };
         };
         // Helper function to get diagonal arrays 
         const diagonalArray = array => array.map((value, index) => value[index]);
         // Helper arrays for diagonals
-        let diagonalRight = diagonalArray(gameBoard.board().reverse());
-        let diagonalLeft = diagonalArray(gameBoard.board());
+        const diagonalRight = diagonalArray(gameBoard.getBoard().reverse());
+        const diagonalLeft = diagonalArray(gameBoard.getBoard());
         // Check Rows and Columns  
-        for (let i = 0; i < gameBoard.board().length; i++) {
-            let row = gameBoard.board()[i];
+        for (let i = 0; i < gameBoard.getBoard().length; i++) {
+            const row = gameBoard.getBoard()[i];
             // Helper array for columns
-            let columnArray = gameBoard.board().map(column => column[i]);
+            const columnArray = gameBoard.getBoard().map(column => column[i]);
             if(compareMarker(row)) return true;
             if(compareMarker(columnArray)) return true;
         };
@@ -79,7 +103,13 @@ const game = (function(){
         if(compareMarker((diagonalLeft))) return true;
         return false;
     };
-    return {gameLoop};
+    const isTie = function() {
+        if (gameBoard.availableCells() === 0){
+            console.log(`It's a tie`);
+            return true;
+        };
+    };
+    return {playRound};
 })();
 
 
